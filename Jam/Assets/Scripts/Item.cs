8c15a26242a02b54c7ec417b5dd03c3684da2ba5
Taylor <taylor.horsman@ontariotechu.net>
2020-10-17 13:18:51 -0400

    Changed Button Names
    Changed UI button names to make life easier

--- Jam/Assets/Scripts/Item.cs
+++ Jam/Assets/Scripts/Item.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+
+    public Button next, last;
+
     private int itemTag;
     private string itemName;
 
