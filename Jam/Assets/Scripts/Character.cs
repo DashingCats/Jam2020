cf4d56fee34730a250a6f508d30b2ea7ce586b59
Daniel <daniel.carr@ontariotechu.net>
2020-10-17 12:13:55 -0400

    Item and Character
    added basic classes for the Items and Characters

---
+++ Jam/Assets/Scripts/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private int charTag;
+    private string charName;
+
+    public int GetTag()
+    {
+        return charTag;
+    }
+
+    public void SetTag(int i)
+    {
+        charTag = i;
+    }
+
+    public string GetName()
+    {
+        return charName;
+    }
+
+    public void SetName(string s)
+    {
+        charName = s;
+    }
+}
