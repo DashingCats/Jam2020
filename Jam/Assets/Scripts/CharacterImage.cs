3940cb92b1783df83a73e36bd34378c69f7bbd54
Taylor <taylor.horsman@ontariotechu.net>
2020-10-17 16:23:37 -0400

    Image Appearence
    Character (WIP, needs to change once the use matches an object to the right character)
    Items -> Allows the user to switch between objects

---
+++ Jam/Assets/Scripts/CharacterImage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterImage : MonoBehaviour
+{
+    public Sprite character1, character2, character3, character4, character5;
+    public int imgnum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void imgchange()
+    {
+        switch (imgnum)
+        {
+            case 0:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character1;
+                break;
+            case 1:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character2;
+                break;
+            case 2:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character3;
+                break;
+            case 3:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character4;
+                break;
+            case 4:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = character5;
+                break;
+           
+        }
+    }
+
+}
---
+++ Jam/Assets/Scripts/ItemImage.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemImage : MonoBehaviour
+{
+    public Sprite cane, dog, headphones, pen, pencilcase, wallet, bike, laptop, textbook, carKeys;
+    public int imgnum = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = cane;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void imgchange()
+    {
+        switch (imgnum)
+        {
+            case 0:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = cane;
+                break;
+            case 1:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = dog;
+                break;
+            case 2:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = headphones;
+                break;
+            case 3:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = pen;
+                break;
+            case 4:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = pencilcase;
+                break;
+            case 5:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = wallet;
+                break;
+            case 6:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = bike;
+                break;
+            case 7:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = laptop;
+                break;
+            case 8:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = textbook;
+                break;
+            case 9:
+                this.gameObject.GetComponent<UnityEngine.UI.Image>().sprite = carKeys;
+                break;
+        }
+    }
+    public void imgIncrease()
+    {
+        imgnum++;
+
+        if (imgnum > 9)
+            imgnum = 0;
+
+        imgchange();
+    }
+
+    public void imgDecrease()
+    {
+        imgnum--;
+
+        if (imgnum < 0)
+            imgnum = 9;
+
+        imgchange();
+    }
+
+}
